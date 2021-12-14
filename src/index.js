a33e1b008d745f28ab1b7e46e4591be44c0e94a7
ercumentlacin <ercument.lacin@akinon.com>
2021-12-14 20:02:23 +0300

    add devto

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ async function getSmashingMagazine(props) {
   });
   return articles;
 }
+
 async function getCssTrick(props) {
   const { page, category, site = 'css-tricks' } = props;
 
@@ -54,6 +55,7 @@ async function getCssTrick(props) {
   });
   return articles;
 }
+
 async function getHashNode(props = {}) {
   const { category = 'javascript', site = 'hashnode' } = props;
   const url = `  https://hashnode.com/n/ajax/${category}/content?type=hot&postType=story&limit=15`;
@@ -105,7 +107,62 @@ async function getHashNode(props = {}) {
   return articles;
 }
 
-const pipe = [getSmashingMagazine, getCssTrick, getHashNode];
+async function getDevTo(props = {}) {
+  const { category = 'javascript', site = 'devto' } = props;
+  const url = `https://dev.to/search/feed_content?per_page=15&page=0&tag=${category}t&sort_by=published_at&sort_direction=desc&tag_names%5B%5D=${category}t&approved=&class_name=Article`;
+  const articles = [];
+  const date_option = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
+
+  try {
+    const { data } = await axios({
+      headers: {
+        accept: 'application/json',
+        'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+        'content-type': 'application/json',
+        'sec-ch-ua':
+          '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'sec-gpc': '1',
+        'x-csrf-token':
+          '26xLGQoBNWb4nSUYeCLENwdmzkj89nA4NnAfJJQU0JK+tmh4t9CrcRGdibw5VDouLQBooIg4iqIHJYKQcqMG/A==',
+      },
+      referrer: 'https://dev.to/t/react/latest',
+      referrerPolicy: 'strict-origin-when-cross-origin',
+      body: null,
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include',
+      url,
+    });
+
+    articles.push(
+      ...data.result.map((post) => ({
+        title: post.title,
+        link: `https://dev.to${post.path}`,
+        date: new Date(post.published_at_int).toLocaleDateString(
+          'en-us',
+          date_option
+        ),
+        site,
+      }))
+    );
+  } catch (error) {
+    console.log(error);
+  }
+
+  return articles;
+}
+
+const pipe = [getSmashingMagazine, getCssTrick, getHashNode, getDevTo];
 
 async function getArticles(props = {}) {
   const { page = '', category = 'react', _articles = [], site } = props;
@@ -131,7 +188,7 @@ async function getArticles(props = {}) {
   };
 }
 
-getArticles({ page: 1, category: 'javascript' }).then(({ data, category }) => {
+getArticles({ page: 1, category: 'css' }).then(({ data, category }) => {
   fs.writeFileSync(
     `./public/json/${category}.json`,
     JSON.stringify(data, null, 2)
