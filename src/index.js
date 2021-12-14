58889643b32234c5b0053e132008bb89af3a537e
ercumentlacin <ercument.lacin@akinon.com>
2021-12-14 12:02:16 +0300

    add hashnode

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function getSmashingMagazine({ page, category, site }) {
+async function getSmashingMagazine(props) {
+  const { page, category, site = 'smashingmagazine' } = props;
+
   const host = `https://www.smashingmagazine.com/category/${category}/`;
   const url = !!page && page > 1 ? `${host}/page/${page}/` : host;
 
@@ -27,7 +29,9 @@ async function getSmashingMagazine({ page, category, site }) {
   });
   return articles;
 }
-async function getCssTrick({ page, category, site }) {
+async function getCssTrick(props) {
+  const { page, category, site = 'css-tricks' } = props;
+
   const host = `https://css-tricks.com/tag/${category}/`;
   const url = !!page && page > 1 ? `${host}/page/${page}/` : host;
 
@@ -50,18 +54,66 @@ async function getCssTrick({ page, category, site }) {
   });
   return articles;
 }
+async function getHashNode(props = {}) {
+  const { category = 'javascript', site = 'hashnode' } = props;
+  const url = `  https://hashnode.com/n/ajax/${category}/content?type=hot&postType=story&limit=15`;
+  const articles = [];
+  const date_option = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
+
+  try {
+    const { data } = await axios({
+      headers: {
+        accept: '*/*',
+        'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+        'sec-ch-ua':
+          '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'sec-gpc': '1',
+      },
+      referrer: 'https://hashnode.com/n/javascript',
+      referrerPolicy: 'strict-origin-when-cross-origin',
+      body: null,
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include',
+      url,
+    });
+
+    articles.push(
+      ...data.posts.map((post) => ({
+        title: post.title,
+        link: `${
+          post.publication.domain || post.publication.username + 'hashnode.dev'
+        }/${post.slug}`,
+        date: new Date(post.dateAdded).toLocaleDateString('en-us', date_option),
+        site,
+      }))
+    );
+  } catch (error) {
+    console.log(error);
+  }
+
+  return articles;
+}
+
+const pipe = [getSmashingMagazine, getCssTrick, getHashNode];
 
 async function getArticles(props = {}) {
   const { page = '', category = 'react', _articles = [], site } = props;
 
-  const callbacks = {
-    smashingmagazine: getSmashingMagazine,
-    'css-tricks': getCssTrick,
-  };
-
-  const articles = await callbacks[site]({ page, category, site });
-
-  _articles.push(...articles);
+  for (const cb of pipe) {
+    const articles = await cb({ page, category, site });
+    _articles.push(...articles);
+  }
 
   if (page < 5 || page === '' || page === 1) {
     return getArticles({
@@ -71,6 +123,7 @@ async function getArticles(props = {}) {
       site,
     });
   }
+
   return {
     data: _articles,
     category,
@@ -78,11 +131,9 @@ async function getArticles(props = {}) {
   };
 }
 
-getArticles({ page: 1, category: 'javascript', site: 'css-tricks' }).then(
-  ({ data, category, site }) => {
-    fs.writeFileSync(
-      `./public/articles/${site}/${category}.json`,
-      JSON.stringify(data, null, 2)
-    );
-  }
-);
+getArticles({ page: 1, category: 'javascript' }).then(({ data, category }) => {
+  fs.writeFileSync(
+    `./public/json/${category}.json`,
+    JSON.stringify(data, null, 2)
+  );
+});
